06e7c62de0eb58fdfff3e49456b8aaa5977e889a
Milad-Rufai <temilader19@gmail.com>
2023-04-22 19:55:29 +0100

    starwars web page

---
+++ swars-webproject/src/components/MovieWebPage.js
@@ -0,0 +1,14 @@
+import React from "react";
+import Post from "./Post";
+import Logo from "./Logo";
+
+const MovieWebPage = () => {
+  return (
+    <div>
+      <Logo />
+      <Post />
+    </div>
+  );
+};
+
+export default MovieWebPage;
